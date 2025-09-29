0e0f6874702bf7f53517625701886bbe8c24990f
oksanapetunina <oksanapetunina@gmail.com>
2025-09-29 23:19:11 +0500

    lesson3

--- lesson_3_task_2.py
+++ lesson_3_task_2.py
@@ -1,18 +1,18 @@
-from smartphone import smartphone
+from Smartphone import Smartphone
 
 
 catalog = [
-    smartphone(phone_brand="Xiaomi", phone_model="Poco",
+    Smartphone(phone_brand="Xiaomi", phone_model="Poco",
                subscriber_number="«+79999999999»"),
-    smartphone(phone_brand="Realme", phone_model="GT2",
+    Smartphone(phone_brand="Realme", phone_model="GT2",
                subscriber_number="«+79998887766»"),
-    smartphone(phone_brand="Samsung", phone_model="Galaxu",
+    Smartphone(phone_brand="Samsung", phone_model="Galaxu",
                subscriber_number="«+79987654321»"),
-    smartphone(phone_brand="Xiaomi", phone_model="Realme",
+    Smartphone(phone_brand="Xiaomi", phone_model="Realme",
                subscriber_number="«+79123456789»"),
-    smartphone(phone_brand="Apple", phone_model="16 Pro",
+    Smartphone(phone_brand="Apple", phone_model="16 Pro",
                subscriber_number="«+79005553535»"),
-    smartphone(phone_brand="Tecno", phone_model="Spark",
+    Smartphone(phone_brand="Tecno", phone_model="Spark",
                subscriber_number="«+79999999999»")
 ]
 
