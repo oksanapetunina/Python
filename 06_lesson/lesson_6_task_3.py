ba727a16370cdc878890ebc71291f4cc2cdccc0f
oksanapetunina <oksanapetunina@gmail.com>
2025-12-15 13:57:26 +0500

    06K_lesson

--- 06_lesson/lesson_6_task_3.py
+++ 06_lesson/lesson_6_task_3.py
@@ -9,8 +9,16 @@ driver.get(
     )
 
 
-img_element = WebDriverWait(driver, 20).until(
+element = WebDriverWait(driver, 10).until(
+    EC.presence_of_all_elements_located((By.CLASS_NAME, "col-12"))
+)
+
+img_element = WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.CSS_SELECTOR, "#award"))
 )
+
 src_value = img_element.get_attribute("src")
 print(src_value)
+
+
+driver.quit()
